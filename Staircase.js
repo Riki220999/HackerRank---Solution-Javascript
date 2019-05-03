9e6099fd3e7956ba71ac821ac9074285c8d19f92
Riki setiyo Pambudi <setiyoriki589@gmail.com>
2019-05-03 21:41:59 +0700

    solution Staircase

---
+++ Staircase.js
@@ -0,0 +1,17 @@
+/**
+Author: Riki Setiyo Pambudi
+make Javascript Nodejs
+*/
+
+function staircase(n) {
+    if (n > 0 && n <= 100 && typeof n === "number" && n === parseInt(n, 0)) {
+        for (let r = 1; r <= n; r++) {
+            let blanks = [...[], ...Array(n - r)].map(i => ' ');
+            let hashes = [...[], ...Array(n - (n - r))].map(i => '#');
+            console.log([
+                ...blanks,
+                ...hashes
+            ].join(''));
+        }
+    } 
+}
